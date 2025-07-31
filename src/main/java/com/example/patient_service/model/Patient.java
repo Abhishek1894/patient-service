5a508ebe2610281eae173f8841c6ba65e713a430
abhishek-ainapur1894 <abiainapur24@gmail.com>
2025-07-31 19:40:40 +0530

    start of patient-service microservice

---
+++ src/main/java/com/example/patient_service/model/Patient.java
@@ -0,0 +1,36 @@
+package com.example.patient_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Patient
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private LocalDateTime dateOfBirth;
+
+    @Column(nullable = false)
+    private LocalDateTime registeredDate;
+}
