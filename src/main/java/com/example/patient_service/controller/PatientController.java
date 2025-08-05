887bc78f94e5e7932c4bbc40ce91b186c700b945
abhishek-ainapur1894 <abiainapur24@gmail.com>
2025-08-06 03:09:00 +0530

    patient-service completed

--- src/main/java/com/example/patient_service/controller/PatientController.java
+++ src/main/java/com/example/patient_service/controller/PatientController.java
@@ -6,6 +6,8 @@ import com.example.patient_service.exception.EmailAlreadyExitsException;
 import com.example.patient_service.exception.PatientNotFoundException;
 import com.example.patient_service.service.PatientService;
 import com.example.patient_service.validators.CreatePatientValidationGroup;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +20,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patients", description = "API for managing Patients")
 public class PatientController
 {
     @Autowired
     PatientService patientService;  // field injection
 
     @GetMapping("")
+    @Operation(summary = "Get all patients")
     public ResponseEntity<List<PatientResponseDTO>> getPatients()
     {
         List<PatientResponseDTO> list = patientService.getPatients();
@@ -31,12 +35,15 @@ public class PatientController
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get a patient by id")
     public ResponseEntity<PatientResponseDTO> getPatientById(@PathVariable long id) throws PatientNotFoundException
     {
         return new ResponseEntity<>(patientService.getPatientById(id), HttpStatus.OK);
     }
 
+
     @PostMapping("")
+    @Operation(description = "Add a Patient")
     public ResponseEntity<PatientResponseDTO> addPatient(@Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO) throws EmailAlreadyExitsException
     {
         PatientResponseDTO patientResponseDTO = patientService.addPatient(patientRequestDTO);
@@ -44,6 +51,7 @@ public class PatientController
     }
 
     @PutMapping("/{id}")
+    @Operation(description = "Update a Patient")
     public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable long id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO) throws PatientNotFoundException
     {
         PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
@@ -52,6 +60,7 @@ public class PatientController
 
 
     @DeleteMapping("/{id}")
+    @Operation(description = "Delete a Patient")
     public ResponseEntity<PatientResponseDTO> deletePatient(@PathVariable long id) throws PatientNotFoundException
     {
         PatientResponseDTO patientResponseDTO = patientService.deletePatient(id);
